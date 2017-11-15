7359b1573b080a36687e4451738fa5ec9130ac76
Andrea Chen <andreachen.ac@gmail.com>
2017-11-14 18:04:19 -0800

    Add score function

--- scrabble.js
+++ scrabble.js
@@ -1,9 +1,51 @@
-const Scrabble = {
-  score: function(word) {
-    // TODO: implement score
-  }
+const SCORECHART = {
+      'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+      'D': 2, 'G': 2,
+      'B': 3, 'C': 3, 'M': 3, 'P': 3,
+      'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+      'K': 5,
+      'J': 8, 'X': 8,
+      'Q': 10, 'Z': 10
+    };
 
-  // TODO: add the highestScoreFrom method
+const Scrabble = {
+  score: function score(word) {
+    // ensure word is a string
+    if (!/^[a-zA-Z]+$/.test(word)) {
+      throw new ArgumentException('Input must only contain letters');
+    } else if (word.length > 7) {
+      throw new ArgumentException('Input must be a max of 7 characters long');
+    }
+    // if (typeof word !== 'string') {
+    //   throw 'Input must be a string';
+    // }
+
+    let wordUpper = word.toUpperCase();
+    let wordScore = 0;
+
+    for (let i = 0; i < wordUpper.length; i++) {
+      if (SCORECHART.hasOwnProperty(wordUpper[i])) {
+        // key = wordUpper[i];
+        wordScore += SCORECHART[wordUpper[i]];
+      }
+    }
+
+    if (wordUpper.length === 7) {
+      wordScore += 50;
+    }
+
+    return wordScore;
+  },
+
+  highestScoreFrom: function highestScoreFrom(words) {
+    let highestScore = words[0];
+    words.forEach(function(word)) {
+      if (highestScore < Scrabble.score(word)) {
+
+      }
+    }
+
+  }
 
 };
 
