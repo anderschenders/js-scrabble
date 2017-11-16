ac95e83bcaf0683077f045129d993d2fa3d62231
Andrea Chen <andreachen.ac@gmail.com>
2017-11-15 16:25:28 -0800

    Create TileBag class with constructor

--- scrabble.js
+++ scrabble.js
@@ -76,7 +76,6 @@ const Scrabble = {
     // })
     return highestScoreWord;
   }
-
 };
 
 Scrabble.Player = class Player {
@@ -138,7 +137,27 @@ Scrabble.Player = class Player {
   highestScoringWord() {
     return Scrabble.highestScoreFrom(this.plays);
   }
+};
 
+Scrabble.TileBag = class TileBag {
+  constructor(){
+    let tiles = {
+      'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 1, 'X': 1, 'Y': 2, 'Z': 1
+    };
+    let tilesArray = [];
+
+    let nestedArray = Object.entries(tiles);
+
+    for (element of nestedArray) {
+      let count = element[1];
+      while (count > 0) {
+        tilesArray.push(element[0]);
+        count--;
+      }
+    }
+
+    this.allTiles = tilesArray;
+  }
 };
 
 module.exports = Scrabble;
